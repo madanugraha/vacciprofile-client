2dbeb0e701dff349a043a29d7fc48d2d8ab2dfbb
steffilamarca <174576295+steffilamarca@users.noreply.github.com>
2024-08-06 12:42:37 +0800

    testing change

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,7 +16,7 @@ const Header = () => {
     return <div className='row bg-primary text-white py-4 slide-down'>
     <div className='col-12'>
         <h3 className='subheading mb-0'>Welcome to</h3>
-        <h1 className='heading'>VacciPROFILE</h1>
+        <h1 className='heading'>VacciPROFILE (changed)</h1>
     </div>
 </div>
 }
