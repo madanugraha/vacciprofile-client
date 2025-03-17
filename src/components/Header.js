c40c839856a95078dd75f47c8517c017da12ff7c
kalleriakronos24 <madacool22@gmail.com>
2025-03-17 16:09:38 +0800

    ..

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,10 +15,10 @@ import moment from 'moment';
 const Header = () => {
     return <div className='row bg-primary text-white pt-3 pb-2 slide-down'>
         <div className='col-12 w-100'>
-            <h3 className='subheading mb-0'>Welcome to</h3>
+            <h3 className='subheading mb-0' style={{ color: '#D17728' }}>Welcome to</h3>
             <div className='d-inline-flex justify-content-between align-items-center w-100'>
-                <h1 className='heading'>VacciPROFILE</h1>
-                <span>Last updated: {moment().format('DD, MMM YYYY')}</span>
+                <img className='heading' src="/images/vacci-profile-logo.png" alt="vacciprofile logo" width={210} height={100} />
+                <span style={{ color: '#D17728' }}>Last updated: {moment().format('DD, MMM YYYY')}</span>
             </div>
 
         </div>
